f8bc421080a67baa24c73bc240055631c6e165e0
bing1100 <bingxuhu@hotmail.com>
2017-10-12 19:28:40 -0400

    Improved runtime

--- min_terms.py
+++ min_terms.py
@@ -9,50 +9,29 @@ def createExpList(target, exp):
     candidate = 1
     currNum = 2
     while(candidate <= target):
-
         expList.append(int(candidate))
-
         candidate = math.pow(currNum, exp)
-
         currNum = currNum + 1
 
-def genChildren(target):
-    childList = []
+def genChildren(target, hMap):
     i = 0
-
     for i in range(0,len(expList)):
         if (expList[i] <= target[0]):
-            childList.append((target[0]-expList[i], target[1] + 1))
-
-    return childList
+            hMap[target[0]-expList[i]] = target[1] + 1
 
 def min_terms(target, exp):
-
     createExpList(target, exp)
-
-    print(expList)
-
     global termsMap
     termsMap = {}
-
     termsMap[target] = 0
     newTermsMap = {}
 
     while(0 not in termsMap):
-
         for key,value in termsMap.items():
-
-            children = genChildren((key,value))
-            
-            for (k,v) in children:
-
-                newTermsMap[k] = v
-
+            genChildren((key,value), newTermsMap)
         termsMap = newTermsMap.copy()
-
     return termsMap[0]
 
-
-print(min_terms(9,3))
+print(min_terms(100,3))
 
 
