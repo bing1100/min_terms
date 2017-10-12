14119c2162ae27965b6a1cd4606bf06d6d1a8676
bing1100 <bingxuhu@hotmail.com>
2017-10-12 19:39:50 -0400

    Added Comments

--- min_terms.py
+++ min_terms.py
@@ -13,22 +13,30 @@ def createExpList(target, exp):
         candidate = math.pow(currNum, exp)
         currNum = currNum + 1
 
-def genChildren(target, children):
-    i = 0
+def dpStep(target, newTermsMap):
     for i in range(0,len(expList)):
         if (expList[i] <= target[0]):
-            children[target[0]-expList[i]] = target[1] + 1
+            # Add the DP to to the children list
+            newTermsMap[target[0]-expList[i]] = target[1] + 1
 
 def min_terms(target, exp):
+    # Create the exponent list
     createExpList(target, exp)
+    
+    # Init useful variables
     global termsMap
     termsMap = {}
     termsMap[target] = 0
     newTermsMap = {}
 
+    # Step through DP as long as no solution found
     while(0 not in termsMap):
+
         for key,value in termsMap.items():
-            genChildren((key,value), newTermsMap)
+            # Create DP children for current all children in old DP step
+            dpStep((key,value), newTermsMap)
+
+        # Copy new DP step for recursion
         termsMap = newTermsMap.copy()
     return termsMap[0]
 
